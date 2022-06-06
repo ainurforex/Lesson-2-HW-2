eb2b8f5c902b6afbd73ffbe24b86ff25acc31711
Ainur <ainurforex@mail.ru>
2022-06-06 18:19:42 +0500

    Lesson 2 HW 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        float boxer1=78.2f;
+        float boxer2=82.7f;
+        float totalWeight;
+        float deltaWeight;
+        totalWeight=boxer1+boxer2;
+        deltaWeight=Math.abs(boxer1-boxer2);
+        System.out.println("Общий вес бойцов "+totalWeight+ " кг.");
+        System.out.println("Разница в весе между бойцами "+deltaWeight+ " кг.");
     }
 }
